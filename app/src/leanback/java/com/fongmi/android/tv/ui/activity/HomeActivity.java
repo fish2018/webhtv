f6eee0ed8f5783e1af46ecf98cfa3ad8dc108b1a
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-20 07:20:32 +0800

    修复退出无法自动清缓存的问题 (#30)
    * 修复退出不自动清缓存问题Add cache clearing on home exit confirmation
    
    * Remove auto cache clearing on HomeActivity destruction
    
    * Enhance exit process with cache clearing
    
    Added cache clearing functionality on exit and removed auto backup call.

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -935,6 +935,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
     private void confirmExitHome() {
         Task.execute(() -> {
             AppDatabase.backupOnExitSync();
+            if (Setting.isAutoClearCache()) Path.clear(Path.cache());
             Process.killProcess(Process.myPid());
             System.exit(0);
         });
@@ -947,7 +948,6 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
         LiveConfig.get().clear();
         VodConfig.get().clear();
         OkHttp.get().clear();
-        if (Setting.isAutoClearCache()) Path.clear(Path.cache());
         Source.get().exit();
         Server.get().stop();
         super.onDestroy();
